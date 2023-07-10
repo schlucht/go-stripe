33ed44508bf8945cef6fa78fd82dd5d13a33c988
Schlucht <web@jagolo.ch>
2023-07-10 04:28:23 +0200

    Payment in buy implemted

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.19
 require (
 	github.com/go-chi/chi/v5 v5.0.8
 	github.com/go-chi/cors v1.2.1
-	github.com/stripe/stripe-go/v74 v74.22.0
 )
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/stripe/stripe-go/v74 v74.25.0
+)
--- internal/cards/cards.go
+++ internal/cards/cards.go
@@ -3,6 +3,7 @@ package cards
 import (
 	"github.com/stripe/stripe-go/v74"
 	"github.com/stripe/stripe-go/v74/paymentintent"
+	"github.com/stripe/stripe-go/v74/paymentmethod"
 )
 
 type Card struct {
@@ -40,6 +41,27 @@ func (c *Card) CreatePaymentIntent(currency string, amount int) (*stripe.Payment
 		return nil, msg, err
 	}
 	return pi, "", err
+}
+
+func (c *Card) GetPaymentMethod(s string) (*stripe.PaymentMethod, error) {
+	stripe.Key = c.Secret
+
+	pm, err := paymentmethod.Get(s, nil)
+	if err != nil {
+		return nil, err
+	}
+	return pm, nil
+}
+
+func (c *Card) RetrievePaymentIntent(id string) (*stripe.PaymentIntent, error) {
+	stripe.Key = c.Secret
+
+	pi, err := paymentintent.Get(id, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return pi, nil
 }
 
 func cardErrorMessage(code stripe.ErrorCode) string {
